f12c0aa109b192c84ccb5b32e87e37409a40f3f8
Lancelot B-L <lancelot.bernardlepine@gmail.com>
2022-09-09 09:58:16 +0200

    [Prog] Fix Drop

--- RandomSeed/Assets/Scripts/Player/PlayerMovement.cs
+++ RandomSeed/Assets/Scripts/Player/PlayerMovement.cs
@@ -93,10 +93,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             if (!canMove)
                 canMove = true;
+            //Debug.Log("oui");
         }
     }
 
